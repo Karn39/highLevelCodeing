d08b8731f652a627b59576470c88e0569e2a3ab1
karn39 <32904171+Karn39@users.noreply.github.com>
2019-10-16 06:02:59 +0800

    Update How to spend y-our money.cpp

--- How to spend y-our money.cpp
+++ How to spend y-our money.cpp
@@ -6,6 +6,7 @@ struct thing
 {
 	int cost;
 	int cp;
+	int value;
 };
 
 int main()
@@ -17,44 +18,62 @@ int main()
 	cin >> m;
 		
 	vector<thing> shop;
+	vector<bool> take(m, false);
 	while (m--)
 	{
 		thing v;
 		cin >> v.cost >> v.cp;
+		v.value = v.cost*v.cp;
 		shop.push_back(v);
 	}
 
 	int first = 0;
 	int add = 1;
 	int maxValue = 0;
-	
+
 	while (1)
 	{
-		if (add > shop.size() - 1)
+		int totalCost = (shop[first].cost <= N) ? shop[first].cost : 0;
+		int nowValue = (shop[first].cost <= N) ? shop[first].value : 0;
+		int thisMax = 0;
+		int Maxpos;
+		bool Balance = false;
+		for (int i = add; i < shop.size(); i++)
+		{			
+			if (totalCost + shop[i].cost <= N && !take[i])
+			{
+				if (nowValue + shop[i].value > thisMax)
+				{
+					thisMax = nowValue + shop[i].value;
+					Maxpos = i;
+					Balance = true;
+				}
+			}
+			if (i == shop.size() - 1)
+			{
+				if (!Balance)
+				{
+					break;
+				}
+				nowValue = thisMax;
+				totalCost = totalCost + shop[Maxpos].cost;
+				take[Maxpos] = true;
+				i = add;
+				Balance = false;
+			}
+		}
+
+		first++;
+		add = first + 1;
+		fill(take.begin(), take.end(), false);
+		if (nowValue > maxValue)
 		{
-			first++;
-			add = first + 1;
+			maxValue = nowValue;
 		}
 		if (first > shop.size() - 1)
 		{
 			cout << maxValue << endl;
 			break;
 		}
-
-		int totalCost = shop[first].cost;
-		int nowValue = shop[first].cost*shop[first].cp;
-		for (int i = add; i < shop.size(); i++)
-		{
-			if (totalCost + shop[i].cost <= N)
-			{
-				totalCost = totalCost + shop[i].cost;
-				nowValue = nowValue + shop[i].cost*shop[i].cp;
-				if (nowValue > maxValue)
-				{
-					maxValue = nowValue;
-				}
-			}
-		}
-		add++;
 	}
 }
